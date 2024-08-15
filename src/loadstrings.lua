fb71cf6b886c39b178ed994af4c17bbb9d3c29b9
alr_dev <157879926+Alr-Dev@users.noreply.github.com>
2024-08-15 19:08:58 -0300

    Create loadstrings.lua

---
+++ src/loadstrings.lua
@@ -0,0 +1,4 @@
+-- Premium version
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Alr-Dev/CEA-HUB/main/src/CEAHUBpremium.lua", true))()
+-- Standard version
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Alr-Dev/CEA-HUB/main/src/CEA-hub1.lua", true))()
